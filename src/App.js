0961292010804c0fc8b4f691d17406220d1d5a04
mcvaliente <maricruz.valiente@gmail.com>
2021-06-07 16:06:34 +0200

    Install material-ui and start sketching the user interface.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React  from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Container } from "@material-ui/core";
+import Header from "./shared/Header";
+import Footer from "./shared/Footer";
+import Home from "./home/Home";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <Header />
+      <Switch>
+        {/* The Switch decides which component to show based on the current URL.*/}
+        <Route exact path="/" component={() => <Home />} />
+      </Switch>
+      <Footer />
+    </Container>
   );
 }
 
