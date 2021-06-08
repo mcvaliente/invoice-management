05e07c83e6632539b31fc3849b53d25fe8009659
mcvaliente <maricruz.valiente@gmail.com>
2021-06-08 13:11:05 +0200

    Create the footer of the pages and add some images.

--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -1,11 +1,31 @@
 import React from "react";
+import styles from "../assets/css/Footer.module.css";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
 
-const Footer = () => {
+export default () => {
+  const srcGitHubImg = "/images/github-icon.svg";
+
   return (
-    <div>
-      <h1>This is footer</h1>
-    </div>
+    <footer className={styles.footer}>
+      <Typography variant="body1" className={styles.footerTypo}>
+        <Link color="inherit" href="https://p2pmodels.eu/" target="_blank">
+          P2P Models
+        </Link>
+        {", "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+      <Typography variant="body2" className={styles.footerTypo}>
+        <Link
+          color="inherit"
+          href="https://github.com/P2PModels"
+          target="_blank"
+          title="GitHub"
+        >
+          <img src={srcGitHubImg} alt="GitHub" />
+        </Link>
+      </Typography>
+    </footer>
   );
 };
-
-export default Footer;
